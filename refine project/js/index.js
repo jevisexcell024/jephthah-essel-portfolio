2d3c9848541c2f3788669a04b9b2ccb364f34188
Jephthah Essel <157823716+jevisexcell024@users.noreply.github.com>
2024-06-15 13:43:35 +0000

    Add files via upload

---
+++ refine project/js/index.js
@@ -0,0 +1,24 @@
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", navToggle);
+
+function navToggle() {
+   navToggler.classList.toggle("active");
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle("open");
+   if(nav.classList.contains("open")){
+     nav.style.maxHeight = nav.scrollHeight + "px";
+   }
+   else{
+     nav.removeAttribute("style");
+   }
+} 
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
